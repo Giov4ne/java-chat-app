3a21807b00fda6775656feba2ed36c0b86fc3212
Giov4ne <giovanewbudal@gmail.com>
2026-04-19 22:46:05 -0300

    feat: add client connection logging with IP and timestamp

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,6 +2,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class Server {
     private static final int PORT = 3000;
@@ -15,6 +19,21 @@ public class Server {
         while(true) {
             Socket socket = server.accept();
             new ClientHandler(socket, users).start();
+            logClientConnection(socket);
         }
+    }
+
+    private static void logClientConnection(Socket socket) throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String formattedDate = now.format(formatter);
+
+        String connectionStr = socket.getInetAddress().getHostAddress() + " - " + formattedDate;
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("logs/client_connections_log.txt", true));
+        writer.write(connectionStr);
+        writer.newLine();
+        writer.close();
     }
 }
